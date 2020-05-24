83dd1c7a3b0b1509b96fb948e29629eab88ef947
unknown <cygan.kontakt@gmail.com>
2020-05-24 11:25:00 +0200

    update

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+
+const clickOn = () => {
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('nav--active');
+}
+hamburger.addEventListener('click', clickOn);
